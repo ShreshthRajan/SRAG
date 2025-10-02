e9d9e468b621a4867be0f6f6be5c8dfa983a8edb
SHRESHTH RAJAN <shreshthrajan@college.harvard.edu>
2025-10-01 23:54:03 -0400

    debugging

--- src/sragv/training/bayesian_pseudo_labeler.py
+++ src/sragv/training/bayesian_pseudo_labeler.py
@@ -27,8 +27,6 @@ logger = logging.getLogger(__name__)
 
 
 
-
-
 class BayesianPseudoLabeler:
     """
     Advanced Bayesian pseudo-labeling with uncertainty quantification and adaptive thresholds.
